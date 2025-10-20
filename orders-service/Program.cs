2226762c04e2ebd48cb8364ffc21458e202a2d4f
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-10-20 16:08:34 -0300

    feat(project): add webapi

---
+++ orders-service/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+
+app.Run();
+
